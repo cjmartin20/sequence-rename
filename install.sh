ed6a40e1eb40ca988f67feed186b479ab0bcdb3a
Colton Martin <cjmartin@eastex.net>
2021-08-30 16:52:28 -0500

    Install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+
+mkdir /opt/Sequence\ Rename
+cp ./seqren.s /opt/Sequence\ Rename/
+echo "alias seqren='/opt/Sequence Rename/seqren.sh'" >> ~/.bashrc
+echo "Script is located at \"opt/Sequence Rename/seqren.sh\""
+echo "~/.bashrc updated"
+echo "Run\n> seqren -h\nTo check for functionality"
