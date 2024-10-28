fb4ca2b5ac70979bf52d5e9860483fe282b220b0
cjmartin20 <cjmartin@eastex.net>
2024-10-28 01:10:16 -0700

    updated install path

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 
-sudo mkdir /opt/Sequence\ Rename
-sudo cp ./seqren.sh /opt/Sequence\ Rename/
-echo "alias seqren='/opt/Sequence Rename/seqren.sh'" >> ~/.bashrc
-echo "Script is located at \"opt/Sequence Rename/seqren.sh\""
+sudo mkdir /home/cjmartin/installed-scripts/sequence-rename/
+sudo cp ./seqren.sh /home/cjmartin/installed-scripts/sequence-rename/
+sudo chmod +x /home/cjmartin/installed-scripts/sequence-rename/seqren.sh
+echo "alias seqren='/home/cjmartin/installed-scripts/sequence-rename/seqren.sh'" >> ~/.bashrc
+echo "Script is located at \"/home/cjmartin/installed-scripts/sequence-rename/\""
 echo "~/.bashrc updated"
 echo "Run\n> seqren -h\nTo check for functionality"
